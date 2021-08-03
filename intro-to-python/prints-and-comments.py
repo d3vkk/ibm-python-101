e3d0c86216d9259c75997c9f68aa31a67d578921
d3vkk <donaldkk22@yahoo.com>
2021-08-03 15:59:36 +0300

    init

---
+++ intro-to-python/prints-and-comments.py
@@ -0,0 +1,25 @@
+print("Hello World")
+
+print("Python is awesome!")
+
+# Single line comment
+# print("Hello World")
+
+# Multi line comment
+'''
+comment 1
+comment 2
+comment 3
+'''
+
+# Next line space
+print("Hello")
+print("World")
+print("Hello\nWorld")
+
+# Tab space
+print("\tHello World")
+
+# Single and double quatation marks
+print("Python\nIs\nAwesome!")
+print('Python\nIs\nAwesome!')
