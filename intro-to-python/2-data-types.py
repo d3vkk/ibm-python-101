fd2dc7f75a9cc63af585b9e4ac8bd54a50742a45
d3vkk <donaldkk22@yahoo.com>
2021-08-03 16:14:11 +0300

    feat: Data types

---
+++ intro-to-python/2-data-types.py
@@ -0,0 +1,18 @@
+# Print data type
+print(type(201))
+print(type(12.7))
+print(type(False))
+print(type(31))
+print(type("True"))
+
+# printing a string, "Hello"
+print("Hello")
+
+# printing an int, 7
+print(7)
+
+# printing a float, 1.5
+print(1.5)
+
+# printing a bool, True
+print(True)
